c2873f715ec3e6679430d94c67812e18e7f8b1d0
Ayoub LAMRANI ALAOUI <lamray17@b042-27.crio.dauphine.fr>
2018-03-29 13:39:15 +0200

    Final version of Contract1 and Contract2

--- src/io/github/oliviercailloux/y2018/xmgui/contract1/MCProblem.java
+++ src/io/github/oliviercailloux/y2018/xmgui/contract1/MCProblem.java
@@ -2,87 +2,75 @@ package io.github.oliviercailloux.y2018.xmgui.contract1;
 
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Objects;
+
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
 
 /**
- * An object of this class MCProblem contains a set of alternatives, criteria and
- * an evaluation table in order to store a complete multi-criteria problem for decision making.
+ * An object of this class MCProblem contains a set of alternatives, criteria
+ * and an evaluation table in order to store a complete multi-criteria problem
+ * for decision making.
  */
 public class MCProblem {
 
-	// Create a set of Alternative objects
-	private Set <Alternative> alternatives = new HashSet <Alternative> ();
-	
-	// Create a set of Criterion objects
-	private Set <Criterion> criteria = new HashSet <Criterion> ();
-	
-	// Create a Guava Table of Alternative and Criterion objects, 
-	// each pair being associated with a value (double)
+	private Set<Alternative> alternatives = new HashSet<Alternative>();
+	private Set<Criterion> criteria = new HashSet<Criterion>();
 	private Table<Alternative, Criterion, Double> tableEval = HashBasedTable.create();
-		
+
 	/**
 	 * Add a value for an alternative-criterion pair
-	 * @param alt, c and val must not be null
+	 * 
+	 * @param alt,
+	 *            c and val must not be null
 	 */
 	public void putValue(Alternative alt, Criterion c, double val) {
-		if (alt == null) {
-			throw new NullPointerException("alt can't be null");
-		}
-		if (c == null) {
-			throw new NullPointerException("c can't be null");
-		}
-		this.alternatives.add(alt);
-		this.criteria.add(c);
-		tableEval.put(alt, c, val);	
-		}
-	
+		this.alternatives.add(Objects.requireNonNull(alt));
+		this.criteria.add(Objects.requireNonNull(c));
+		tableEval.put(alt, c, val);
+	}
+
 	/**
 	 * Add an alternative to the Set
-	 * @param alt must not be null
+	 * 
+	 * @param alt
+	 *            must not be null
 	 */
 	public void addAlt(Alternative alt) {
-		if (alt == null) {
-			throw new NullPointerException("alt can't be null");
-		}
-		this.alternatives.add(alt);
+		this.alternatives.add(Objects.requireNonNull(alt));
 	}
-	
+
 	/**
 	 * Add a criterion to the Set
-	 * @param c must not be null
+	 * 
+	 * @param c
+	 *            must not be null
 	 */
 	public void addAlt(Criterion c) {
-		if (c == null) {
-			throw new NullPointerException("c can't be null");
-		}
-		this.criteria.add(c);
+		this.criteria.add(Objects.requireNonNull(c));
 	}
 
 	/**
 	 * Accessor for an alternative-criterion pair value in the tableEval
-	 * @param alt and c must not be null and must be present in the tableEval
+	 * 
+	 * @param alt
+	 *            and c must not be null and must be present in the tableEval
 	 */
 	public double getValue(Alternative alt, Criterion c) {
-		if (alt == null) {
-			throw new NullPointerException("alt can't be null");
+		if (!tableEval.contains(Objects.requireNonNull(alt), Objects.requireNonNull(c))) {
+			throw new IllegalArgumentException("the alt-c pair does not exist in the tableEval");
 		}
-		if (c == null) {
-			throw new NullPointerException("c can't be null");
-		}
-		if (!tableEval.contains(alt, c)) {
-			throw new IllegalArgumentException ("the alt-c pair does not exist in the tableEval");
-		}
-		return tableEval.get(alt, c);
+		return tableEval.get(Objects.requireNonNull(alt), Objects.requireNonNull(c));
 	}
 
 	/**
 	 * Accessor for the whole tableEval
+	 * 
 	 * @return a copy of the tableEval
 	 */
 	public Table<Alternative, Criterion, Double> getTableEval() {
-		Table<Alternative, Criterion, Double> tempTableEval = tableEval; 
+		Table<Alternative, Criterion, Double> tempTableEval = tableEval;
 		return tempTableEval;
 	}
-	
+
 }
--- src/io/github/oliviercailloux/y2018/xmgui/contract2/AlternativesRanking.java
+++ src/io/github/oliviercailloux/y2018/xmgui/contract2/AlternativesRanking.java
@@ -1,6 +1,5 @@
 package io.github.oliviercailloux.y2018.xmgui.contract2;
 
-import java.text.SimpleDateFormat;
 import java.util.*;
 
 import com.google.common.collect.HashMultimap;
@@ -14,31 +13,34 @@ import io.github.oliviercailloux.y2018.xmgui.contract1.Alternative;
  */
 public class AlternativesRanking {
 
-	// Create a multimap to store alternative-rank associations
 	private SetMultimap<Integer, Alternative> map = HashMultimap.create();
-	// Create a multimap to store an history of operations performed on map
-	private SetMultimap<Integer, String> history = HashMultimap.create();
 
 	/**
-	 * Put a rank for a given alternative in order to construct and AlternativeRanking instance
-	 * This also creates a history entry for the put alternative.
-	 * @param rank must be positive
-	 * @param alt must not be null
+	 * Put a rank for a given alternative in order to construct and
+	 * AlternativeRanking instance This also creates a history entry for the put
+	 * alternative.
+	 * 
+	 * @param rank
+	 *            must be positive
+	 * @param alt
+	 *            must not be null
 	 */
 	public AlternativesRanking(int rank, Alternative alt) {
 		if (rank <= 0 || alt == null)
 			throw new IllegalArgumentException("rank must be positive and alt must not be null");
-		// Create an history for this AlternativesRanking instance
-		history("Inserted by constructor AlternativeId : " + alt.getId());
 		map.put(rank, alt);
 
 	}
 
 	/**
-	 * Put a rank for a given alternative. Can be used to put ex-aequo alternatives on the same rank.
-	 * This also creates a history entry for the put alternative.
-	 * @param rank must be positive and inferior to the total number of alternatives + 1
-	 * @param alt must not be null
+	 * Put a rank for a given alternative. Can be used to put ex-aequo alternatives
+	 * on the same rank. This also creates a history entry for the put alternative.
+	 * 
+	 * @param rank
+	 *            must be positive and inferior to the total number of alternatives
+	 *            + 1
+	 * @param alt
+	 *            must not be null
 	 */
 	public void putAltRank(int rank, Alternative alt) {
 		if (rank <= 0 || alt == null) {
@@ -46,14 +48,16 @@ public class AlternativesRanking {
 		}
 		// Validate the rank given is not too big
 		validateRank(rank);
-		history("Inserted by putAltRank AlternativeId : " + alt.getId());
 		map.put(rank, alt);
 	}
 
 	/**
 	 * Alternative accessor. Return the corresponding alternative(s) for a given
 	 * rank.
-	 * @param rank must be positive and inferior to the total number of alternatives + 1
+	 * 
+	 * @param rank
+	 *            must be positive and inferior to the total number of alternatives
+	 *            + 1
 	 * @return alternative(s)
 	 */
 	public Set<Alternative> getAlt(int rank) {
@@ -71,57 +75,40 @@ public class AlternativesRanking {
 	}
 
 	/**
-	 * Remove all entries of a given alternative from the map.
-	 * This also creates a history entry for the removed alternative.
-	 * @param alt must not be null
+	 * Remove all entries of a given alternative from the map. This also creates a
+	 * history entry for the removed alternative.
+	 * 
+	 * @param alt
+	 *            must not be null
 	 */
 	public void removeAlt(Alternative alt) {
 		if (alt == null)
 			throw new IllegalArgumentException("alt must not be null");
-		for (int i: map.keys()) {
+		for (int i : map.keys()) {
 			for (Alternative a : map.get(i))
 				if (a.getId() == alt.getId()) {
 					map.remove(i, alt);
 				}
 		}
-		history("Remove by removeAlt AlternativeId : " + alt.getId());
-	}
-	
-	/**
-	 * Validate that the rank about to be put is inferior to the number of alternatives + 1.
-	 */
-	private void validateRank(int rank) {
-		if ( (rank > map.KeySet().size() +1) ) {
-			throw new IllegalArgumentException("Given rank is invalid (too big).");
-		}
-	}
-	
-	/**
-	 * Add a timestamped history entry.
-	 */
-	public void history(String h) {
-		String sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-				.format(new Date());
-		String s = "[" + sdf + "] : " + h;
-		history.put(history.size() + 1, s);
 	}
 
-	public String getHistory() {
-		String h = "";
-		for (String val : history.values())
-			h += val + "\n";
-		h += "[ Map size " + map.size() + " ]";
-		return h;
+	/**
+	 * Validate that the rank about to be put is inferior to the number of
+	 * alternatives + 1.
+	 */
+	private void validateRank(int rank) {
+		if ((rank > map.keySet().size() + 1)) {
+			throw new IllegalArgumentException("Given rank is invalid (too big).");
+		}
 	}
 
 	public String toString() {
 		String s = "";
 		for (int i = 0; i <= map.size(); i++) {
 			for (Alternative alt : map.get(i))
-				s += " Rank : " + i + " -> " + " AlternativeId : "
-						+ alt.getId() + "\n";
+				s += " Rank : " + i + " -> " + " AlternativeId : " + alt.getId() + "\n";
 		}
 		return s;
 	}
-	
+
 }
