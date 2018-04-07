cc1aa28b9a5c409cd0687a8b7368d77dad0de3e2
Raphaaal <azorin.raphael@gmail.com>
2018-04-07 17:34:12 +0200

    final unmarshaller

--- src/io/github/oliviercailloux/y2018/xmgui/file1/Unmarshalling2.java
+++ src/io/github/oliviercailloux/y2018/xmgui/file1/Unmarshalling2.java
@@ -12,8 +12,6 @@ import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
-import javax.xml.transform.stream.StreamSource;
-
 
 import io.github.oliviercailloux.xmcda_2_2_1_jaxb.ObjectFactory;
 import io.github.oliviercailloux.xmcda_2_2_1_jaxb.X2Alternative;
@@ -22,6 +20,9 @@ import io.github.oliviercailloux.xmcda_2_2_1_jaxb.X2AlternativesSet;
 import io.github.oliviercailloux.xmcda_2_2_1_jaxb.X2Criteria;
 import io.github.oliviercailloux.xmcda_2_2_1_jaxb.X2Criterion;
 import io.github.oliviercailloux.xmcda_2_2_1_jaxb.XMCDA;
+import io.github.oliviercailloux.xmcda_2_2_1_jaxb.X2PerformanceTable;
+import io.github.oliviercailloux.xmcda_2_2_1_jaxb.X2Value;
+import io.github.oliviercailloux.xmcda_2_2_1_jaxb.X2AlternativeOnCriteriaPerformances;
 import io.github.oliviercailloux.y2018.xmgui.contract1.MCProblem;
 import io.github.oliviercailloux.y2018.xmgui.contract1.Alternative;
 import io.github.oliviercailloux.y2018.xmgui.contract1.Criterion;
@@ -31,13 +32,7 @@ public class Unmarshalling2 {
 	
 	private MCProblem mcp = new MCProblem();
 
-	/* public Unmarshalling2(MCProblem mcp) {
-		this.mcp = mcp;
-		}
-	*/
-	
-	
-	public void unmarshalAndStore() throws JAXBException, FileNotFoundException, IOException {
+	public MCProblem unmarshalAndStore() throws JAXBException, FileNotFoundException, IOException {
 		
 		
 		final JAXBContext jc = JAXBContext.newInstance(XMCDA.class);
@@ -50,11 +45,12 @@ public class Unmarshalling2 {
 			final List<JAXBElement<?>> xmcdaSubElements = xmcda.getProjectReferenceOrMethodMessagesOrMethodParameters();
 			System.out.println(xmcdaSubElements);
 			
+			//////// FOR ALTERNATIVES ///////
 			// Find the index of the xmcdaSubElements list where there are the X2Alternatives
 			int altsIndex = 0;
 			while (altsIndex < xmcdaSubElements.size()) {
 				if ( xmcdaSubElements.get(altsIndex).getName().toString().equalsIgnoreCase("alternatives") ) {
-					break;
+				break;
 				}
 				altsIndex++;
 				} 
@@ -76,13 +72,14 @@ public class Unmarshalling2 {
 				
 			}
 			
+			//////// FOR CRITERIA ///////
 			// Find the index of the xmcdaSubElements list where there are the X2Criteria
 			int critsIndex = 0;
 			while (critsIndex < xmcdaSubElements.size()) {
 				if ( xmcdaSubElements.get(critsIndex).getName().toString().equalsIgnoreCase("criteria") ) {
-						break;
+					break;
 					}
-						critsIndex++;
+				critsIndex++;
 				} 
 			// Visual verification on console		
 			System.out.println(xmcdaSubElements.get(critsIndex).getName());
@@ -100,11 +97,48 @@ public class Unmarshalling2 {
 				//Put it in an MCProlem object
 				mcp.addCrit(c);
 			}
+
+			//////// FOR PERFORMANCE ///////
+			// Find the index of the xmcdaSubElements list where there is the performances table
+			int perfsIndex = 0;
+			while (perfsIndex < xmcdaSubElements.size()) {
+				if ( xmcdaSubElements.get(perfsIndex).getName().toString().equalsIgnoreCase("performanceTable") ) {
+					break;
+					}
+				perfsIndex++;
+				} 
+			// Visual verification on console		
+			System.out.println(xmcdaSubElements.get(perfsIndex).getName());
+			// Get X2PerformanceTable
+			X2PerformanceTable perfTable = (X2PerformanceTable) xmcdaSubElements.get(perfsIndex).getValue();
+			// Get the list of X2Alternative performances
+			List<X2AlternativeOnCriteriaPerformances> altsOnCritsPerf = perfTable.getAlternativePerformances();
+			for (int i = 0; i < altsOnCritsPerf.size(); i++) {
+				X2AlternativeOnCriteriaPerformances firstEvalAlt = altsOnCritsPerf.get(i);
+				// Get the id of the first evaluated X2Alternative
+				String firstEvalAltId = firstEvalAlt.getAlternativeID();
+				Alternative a = new Alternative(Integer.parseInt(firstEvalAltId.substring(1)));
+				List<X2AlternativeOnCriteriaPerformances.Performance> firstEvalAltPerf = firstEvalAlt.getPerformance();
+				// Iterate over all the criteria this first X2Alternative was evaluated on
+				for (int j = 0; j < firstEvalAltPerf.size(); j++) {
+					X2AlternativeOnCriteriaPerformances.Performance firstEvalAltPerfOnCrit = firstEvalAltPerf.get(j);
+					String firstEvalAltPerfOnCritId = firstEvalAltPerfOnCrit.getCriterionID();
+					X2Value firstEvalAltPerfOnCritIdValue = firstEvalAltPerfOnCrit.getValue();
+					// Put the Alternative-Criteria pair in the tableEval
+					Criterion c = new Criterion(Integer.parseInt(firstEvalAltPerfOnCritId.substring(1)));
+					double v = firstEvalAltPerfOnCritIdValue.getReal();
+					mcp.putValue(a, c, v);
+				}
+			}
 		} // end try
+		
+		return mcp;
+				
 	} // end marshalAndStore
 	
 	public static void main(String[] args) throws JAXBException, FileNotFoundException, IOException{
 		Unmarshalling2 u = new Unmarshalling2();
-		u.unmarshalAndStore();
+		MCProblem unmarshalledMcp = u.unmarshalAndStore();
+		System.out.println(unmarshalledMcp.getTableEval());
 	}
 }
